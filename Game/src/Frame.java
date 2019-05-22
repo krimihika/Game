fe06eddb09f031b21ed8c4516697a0b43342af58
krimihika <42474723+krimihika@users.noreply.github.com>
2019-05-22 10:31:54 -0700

    Add files via upload

---
+++ Game/src/Frame.java
@@ -0,0 +1,12 @@
+
+
+import javax.swing.JFrame;
+
+public class Frame {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Mini Tennis");
+		frame.setSize(300, 500);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
